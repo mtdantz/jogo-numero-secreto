3f829a3654906bce586e115b6adf389997a8af77
Mateus Dantas <mateusdantz@gmail.com>
2023-11-30 20:10:59 -0300

    Atualização

--- app.js
+++ app.js
@@ -7,75 +7,88 @@ let paragrafo = "Escolha um numero entre 1 e 10";
 let tentativas = 1;
 
 
-function escreverNoElemento(tag,texto){
+
+function escreverNoElemento(tag, texto) {
     let elemento = document.querySelector(tag);
     elemento.innerHTML = texto;
-    responsiveVoice.speak(texto,'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
-function exibirMensagemInicial(){
+function exibirMensagemInicial() {
     escreverNoElemento('h1', titulo);
     escreverNoElemento('p', paragrafo);
 
 }
 exibirMensagemInicial()
 
-function verificarChute(){ // Função responsável por verificar se o chute é igual ao numero aleatorio
+function verificarChute() {
+    // Função responsável por verificar se o chute é igual ao numero aleatorio
     let chute = document.querySelector('input').value; // o .value é usado para captar apenas o valor da tag input, e armazenar na variavel chute
     if (chute == numAleatorio) {
-        escreverNoElemento('h1','Muito Bom!!!');
+        escreverNoElemento('h1', 'Muito Bom!!!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let textoTentativa = `Você acertou o numero com ${tentativas} ${palavraTentativa}!`;
         escreverNoElemento('p', textoTentativa);
-
+        mudarImagemRobo(estadoImagem.acertou)
         let novoJogo = document.getElementById('reiniciar').removeAttribute('disabled')
-        let chutar = document.querySelector('button').setAttribute('disabled',true)
-        let imagemRobo = document.querySelector('img')
-        imagemRobo.src = '/img/robozinhoS.png'
-        
+        let chutar = document.querySelector('button').setAttribute('disabled', true)
 
-    } else{
-        if(chute > numAleatorio){
+
+
+    } else {
+        if (chute > numAleatorio) {
             escreverNoElemento('p', 'O numero é menor');
-            
-        } else{
+
+        } else {
             escreverNoElemento('p', 'O numero é maior');
         }
-        tentativas ++;
+        tentativas++;
         limparCampo();
     }
 }
 
-function numeroAleatorio(){
+const estadoImagem = {
+    perguntando: "perguntando",
+    acertou: "acertou",    
+}
 
-    let numeroEscolhido = parseInt(Math.random() * numeroLimite +1)
+function mudarImagemRobo(estado) {
+    const imagemRobo = document.querySelector('img')
+
+    if (estado === estadoImagem.perguntando) {
+        imagemRobo.src = './img/robozinhoQ.png'
+    } 
+    if (estado === estadoImagem.acertou) {
+        imagemRobo.src = './img/robozinhoS.png'
+    }
+}
+
+function numeroAleatorio() {
+
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1)
     let quantidadeNumerosSorteados = listaDeNumeros.length;
-
-    if(quantidadeNumerosSorteados == numeroLimite){
+    if (quantidadeNumerosSorteados == numeroLimite) {
         listaDeNumeros = []
     }
-    
-
-    if(listaDeNumeros.includes(numeroEscolhido)){
+    if (listaDeNumeros.includes(numeroEscolhido)) {
         return numeroAleatorio();
-    }else{
+    } else {
         listaDeNumeros.push(numeroEscolhido);
         return numeroEscolhido;
     }
 }
 
-function limparCampo(){
+function limparCampo() {
     chute = document.querySelector('input')
     chute.value = ''
-    
+
 }
-function reiniciarJogo(){
+function reiniciarJogo() {
     numAleatorio = numeroAleatorio();
     limparCampo();
-    tentativas = 1;    
+    tentativas = 1;
     exibirMensagemInicial();
     chutar = document.querySelector('button').removeAttribute('disabled');
-    novoJogo = document.getElementById('reiniciar').setAttribute('disabled',true)
-    
-
+    novoJogo = document.getElementById('reiniciar').setAttribute('disabled', true)
+    mudarImagemRobo(estadoImagem.perguntando)
 }
